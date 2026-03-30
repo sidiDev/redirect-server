c2044cefdd53d04556167aee9dae0aa2930a5d99
sidiDev <sididev3@gmail.com>
2026-03-30 18:04:58 +0000

    Update response status code in custom redirect script from 302 to 307 for temporary redirects

--- custom-redirect-script.py
+++ custom-redirect-script.py
@@ -58,7 +58,7 @@ class RedirectHandler(BaseHTTPRequestHandler):
             f.write(log_entry)
 
         # Send redirect
-        self.send_response(302)
+        self.send_response(307)
         self.send_header("Location", dest_url)
         self.end_headers()
 
